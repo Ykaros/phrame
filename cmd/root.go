2a36dbd4e54463b13ddb91a0a77204162da8421a
Ykaros <jiazong.gong@gmail.com>
2023-11-16 16:34:07 -0500

    Concurrency support

--- cmd/root.go
+++ cmd/root.go
@@ -8,54 +8,31 @@ import (
 	"github.com/Ykaros/phrame/utils"
 	"github.com/spf13/cobra"
 	"os"
-	"strconv"
 )
-
-//var formatOption int
 
 var rootCmd = &cobra.Command{
 	Use:   "phrame",
 	Short: "A CLI tool written in Go to add a frame to photo(s)",
+	// At least a source is required
+	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		sourcePath := args[0]
 
-		sourcePath, _ := cmd.Flags().GetString("source")
-		destinationPath, _ := cmd.Flags().GetString("destination")
+		var destinationPath string
+		// Output path is optional and can be passed as a flag or as an argument
+		if len(args) > 1 {
+			destinationPath = args[1]
+		} else {
+			destinationPath, _ = cmd.Flags().GetString("destination")
+		}
+
 		borderRatio, _ := cmd.Flags().GetFloat64("borderRatio")
 		//colorOption, _ := cmd.Flags().GetBool("color")
 
-		for i, arg := range args {
-			switch i {
-			case 0:
-				sourcePath = arg
-			case 1:
-				destinationPath = arg
-			case 2:
-				borderRatio, _ = strconv.ParseFloat(arg, 64)
-				//case 3:
-				//	colorOption, _ = strconv.ParseBool(arg)
-			}
-		}
-
-		// Check if the source exists and source type
-		fileInfo, err := os.Stat(sourcePath)
+		err := utils.AddFrames(sourcePath, destinationPath, borderRatio)
 		if err != nil {
-			fmt.Printf("Error accessing file(s) at %s: %v\n", sourcePath, err)
-			return
-		}
-
-		// Determine if the source is a directory or a file
-		if fileInfo.IsDir() {
-			err := utils.AddFrames(sourcePath, destinationPath, borderRatio)
-			if err != nil {
-				fmt.Printf("Error adding frames: %v\n", err)
-				os.Exit(1)
-			}
-		} else {
-			err := utils.AddFrame(sourcePath, destinationPath, borderRatio)
-			if err != nil {
-				fmt.Printf("Error adding frames: %v\n", err)
-				os.Exit(1)
-			}
+			fmt.Printf("Error adding frames: %v\n", err)
+			os.Exit(1)
 		}
 	},
 }
--- utils/image.go
+++ utils/image.go
@@ -10,32 +10,59 @@ import (
 	_ "image/png"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
-func AddFrame(imgPath, outPath string, borderRatio float64) error {
-
-	// TODO: Add image validation
-
-	img, err := os.Open(imgPath)
+// TDDO: Image validation
+func readImage(path string) (image.Image, error) {
+	img, err := os.Open(path)
 	if err != nil {
-		fmt.Printf("Error reading image from %s: %v\n", imgPath, err)
+		return nil, err
 	}
-	defer img.Close()
+	defer func(img *os.File) {
+		err := img.Close()
+		if err != nil {
+			//TODO: Handle error
+		}
+	}(img)
 	photo, _, err := image.Decode(img)
 	if err != nil {
-		fmt.Printf("Error reading image from %s: %v\n", imgPath, err)
+		return nil, err
 	}
+	return photo, nil
+}
 
+// TODO: Ask to create dir if there is none
+func saveImage(path string, photo image.Image) error {
+	outIMG, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer func(outIMG *os.File) {
+		err := outIMG.Close()
+		if err != nil {
+
+		}
+	}(outIMG)
+	err = jpeg.Encode(outIMG, photo, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func createCanvas(photo image.Image, borderRatio float64) *image.RGBA {
 	// Determine the size of the border and the square
-	borderWidth := int(borderRatio * float64(min(photo.Bounds().Dx(), photo.Bounds().Dy())))
-	squareSize := max(photo.Bounds().Dx(), photo.Bounds().Dy()) + 2*borderWidth
+	width, height := photo.Bounds().Dx(), photo.Bounds().Dy()
+	borderWidth := int(borderRatio * float64(min(width, height)))
+	squareSize := max(width, height) + 2*borderWidth
 
 	// Create a squared RGBA canvas
 	canvas := image.NewRGBA(image.Rect(0, 0, squareSize, squareSize))
 
-	// Calculate the center point
-	startX := squareSize / 2
-	startY := squareSize / 2
+	// Calculate the starting point
+	startX := (squareSize - width) / 2
+	startY := (squareSize - height) / 2
 
 	// Fill the entire canvas with white
 	// More color options
@@ -43,50 +70,72 @@ func AddFrame(imgPath, outPath string, borderRatio float64) error {
 		&image.Uniform{color.White}, image.Point{}, draw.Over)
 
 	// Place the photo at the center of the canvas
-	draw.Draw(canvas,
-		image.Rect(startX-photo.Bounds().Dx()/2, startY-photo.Bounds().Dy()/2,
-			startX+photo.Bounds().Dx()/2, startY+photo.Bounds().Dy()/2),
+	draw.Draw(canvas, image.Rect(startX, startY, startX+width, startY+height),
 		photo, image.Point{}, draw.Over)
-
-	//TODO: Ask to create dir if there is none
-	outIMG, err := os.Create(outPath)
-	if err != nil {
-		fmt.Printf("Error creating new folder: %v\n", err)
-	}
-	defer outIMG.Close()
-
-	// Encode the image and save it as JPEG
-	err = jpeg.Encode(outIMG, canvas, nil)
-	if err != nil {
-		fmt.Printf("Error saving image: %v\n", err)
-	}
-
-	fmt.Printf("Image successfully saved to: %s\n", outPath)
-
-	return nil
+	return canvas
 }
 
-func AddFrames(sourceDir, destinationDir string, borderRatio float64) error {
-	// Multi-threading support
-	images, err := os.ReadDir(sourceDir)
+//func createDir(path string) error {
+//	return nil
+//}
+
+func AddFrames(sourcePath, outPath string, borderRatio float64) error {
+
+	// Check if the source exists and source type
+	fileInfo, err := os.Stat(sourcePath)
 	if err != nil {
-		fmt.Printf("Error reading files from %s: %v\n", sourceDir, err)
+		fmt.Printf("Error accessing file(s) at %s: %v\n", sourcePath, err)
 	}
 
-	for _, file := range images {
-		//fmt.Println(file)
-		if file.IsDir() || filepath.Ext(file.Name()) == ".DS_Store" {
-			continue
-		}
-
-		imgPath := filepath.Join(sourceDir, file.Name())
-		outPath := filepath.Join(destinationDir, file.Name())
-
-		err := AddFrame(imgPath, outPath, borderRatio)
+	// Determine if the source is a directory or a file
+	if fileInfo.IsDir() {
+		images, err := os.ReadDir(sourcePath)
 		if err != nil {
-			fmt.Printf("Error processing %s: %v\n", imgPath, err)
+			fmt.Printf("Error reading files from %s: %v\n", sourcePath, err)
 		}
-	}
 
+		//channel := make(chan string)
+		var wg sync.WaitGroup
+		for _, file := range images {
+			//fmt.Println(file)
+			if file.IsDir() || filepath.Ext(file.Name()) == ".DS_Store" {
+				continue
+			}
+			wg.Add(1)
+
+			go func(imgPath, savePath string) {
+				defer wg.Done()
+
+				photo, err := readImage(imgPath)
+				if err != nil {
+					fmt.Printf("Error reading image %s: %v\n", imgPath, err)
+					return
+				}
+				canvas := createCanvas(photo, borderRatio)
+				err = saveImage(savePath, canvas)
+				if err != nil {
+					fmt.Printf("Error saving image %s: %v\n", savePath, err)
+					return
+				}
+				fmt.Printf("Image successfully saved to: %s\n", savePath)
+			}(filepath.Join(sourcePath, file.Name()), filepath.Join(outPath, file.Name()))
+		}
+
+		wg.Wait() // Wait for all goroutines to finish
+		return nil
+
+	} else {
+		// Just one image
+		photo, _ := readImage(sourcePath)
+
+		canvas := createCanvas(photo, borderRatio)
+
+		err := saveImage(outPath, canvas)
+		if err != nil {
+			fmt.Printf("Error saving image: %v\n", err)
+		}
+
+		fmt.Printf("Image successfully saved to: %s\n", outPath)
+	}
 	return nil
 }
